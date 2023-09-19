7562923558d6e2dd872073f119d9d84c3409c72e
jpromanonet <programming@jpromano.net>
2023-09-19 10:02:38 -0300

    Initial commit

---
+++ white_board.py
@@ -0,0 +1,8 @@
+# First, let's import tkinter and some module for color manipulation and choosing
+from tkinter import *
+from tkinter.colorchooser import askcolor
+from tkinter import ttk
+import tkinter as tk
+
+# Not, let's define the root
+root = Tk()
