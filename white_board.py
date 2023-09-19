19d16a1f59126192c898e3a04b9750e14a2e27b6
jpromanonet <programming@jpromano.net>
2023-09-19 11:47:54 -0300

    Windows size config

--- white_board.py
+++ white_board.py
@@ -11,5 +11,27 @@ root.geometry("1050x570+150+50")
 root.configure(bg="#f2f3f5")
 root.resizable(False,False)
 
+# Icons and images
+
+## Color bar
+color_box = PhotoImage(file="./assets/side_bar.png")
+Label(root, image=color_box,bg="#f2f3f5").place(x=10,y=20)
+
+# Create the erase
+eraser = PhotoImage(file="./assets/eraser.png")
+Button(root, image=eraser, bg="#f2f3f5").place(x=30, y=400)
+
+# Here we define the colors use sidebar
+colors = Canvas(root, bg="#ffffff", width=37,height=300,bd=0)
+colors.place(x=30, y=60)
+
+# Here it goes the drawing canvas
+canvas = Canvas(root, width=930, height=500, background="white", cursor="hand2")
+canvas.place(x=100,y=10)
+
+# Now let's bind the canvas with the colors and buttons
+canvas.bind("<Button-1>")
+canvas.bind("<B1-Motion>")
+
 # Run the app
 root.mainloop()
