50f83e228f8fcb09281276d65b0cdb1ea13fbdc6
jpromanonet <programming@jpromano.net>
2023-09-19 12:55:58 -0300

    Creating the colors pallete

--- white_board.py
+++ white_board.py
@@ -31,6 +31,38 @@ def display_pallete():
     id = colors.create_rectangle((10,10,30,30), fill="black")
     colors.tag_bind(id, "Button-1", lambda x: show_color("black"))
 
+    # Gray
+    id = colors.create_rectangle((10,40,30,60), fill="gray")
+    colors.tag_bind(id, "Button-1", lambda x: show_color("gray"))
+
+    # Brown
+    id = colors.create_rectangle((10,70,30,90), fill="brown")
+    colors.tag_bind(id, "Button-1", lambda x: show_color("brown"))
+
+    # Red
+    id = colors.create_rectangle((10,100,30,120), fill="red")
+    colors.tag_bind(id, "Button-1", lambda x: show_color("red"))
+
+    # Orange
+    id = colors.create_rectangle((10,130,30,150), fill="orange")
+    colors.tag_bind(id, "Button-1", lambda x: show_color("orange"))
+
+    # Yellow
+    id = colors.create_rectangle((10,160,30,180), fill="yellow")
+    colors.tag_bind(id, "Button-1", lambda x: show_color("yellow"))
+
+    # Green
+    id = colors.create_rectangle((10,190,30,210), fill="green")
+    colors.tag_bind(id, "Button-1", lambda x: show_color("green"))
+
+    # Blue
+    id = colors.create_rectangle((10,220,30,240), fill="blue")
+    colors.tag_bind(id, "Button-1", lambda x: show_color("blue"))
+
+    # Purple
+    id = colors.create_rectangle((10,250,30,270), fill="purple")
+    colors.tag_bind(id, "Button-1", lambda x: show_color("purple"))
+
 display_pallete()
 
 # Here it goes the drawing canvas
