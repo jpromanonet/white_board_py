bac9090e36bf236a1dbb5246419fa5cae0e90bcd
jpromanonet <programming@jpromano.net>
2023-09-19 10:04:59 -0300

    Windows size config

--- white_board.py
+++ white_board.py
@@ -4,5 +4,10 @@ from tkinter.colorchooser import askcolor
 from tkinter import ttk
 import tkinter as tk
 
-# Not, let's define the root
+# Not, let's define the root and the windown configuration like title, size, etc
 root = Tk()
+root.title("White Board")
+root.geometry("1050*570+150+50")
+root.configure(bg="#f2f3f5")
+root.resizable(False,False)
+
