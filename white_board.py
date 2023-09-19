c206847f8178e799fe3cff765f98a1568b58f1ed
jpromanonet <programming@jpromano.net>
2023-09-19 10:06:26 -0300

    Windows size config

--- white_board.py
+++ white_board.py
@@ -7,7 +7,9 @@ import tkinter as tk
 # Now, let's define the root and the windown configuration like title, size, etc
 root = Tk()
 root.title("White Board")
-root.geometry("1050*570+150+50")
+root.geometry("1050x570+150+50")
 root.configure(bg="#f2f3f5")
 root.resizable(False,False)
 
+# Run the app
+root.mainloop()
