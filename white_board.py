5481bb518c5c56e609d7742f4df9dd267f16dbfc
jpromanonet <programming@jpromano.net>
2023-11-06 15:57:12 -0300

    Done

--- white_board.py
+++ white_board.py
@@ -1,82 +1,63 @@
 import tkinter as tk
 from tkinter.colorchooser import askcolor
-from tkinter import ttk
 
-app = tk.Tk()
-app.title("Drawing Canvas")
-app.geometry("1050x570+150+50")
-app.configure(bg="#f2f3f5")
-app.resizable(False, False)
+def start_drawing(event):
+    global is_drawing, prev_x, prev_y
+    is_drawing = True
+    prev_x, prev_y = event.x, event.y
 
-current_x = 0
-current_y = 0
-drawing_color = "black"
+def draw(event):
+    global is_drawing, prev_x, prev_y
+    if is_drawing:
+        current_x, current_y = event.x, event.y
+        canvas.create_line(prev_x, prev_y, current_x, current_y, fill=drawing_color, width=line_width, capstyle=tk.ROUND, smooth=True)
+        prev_x, prev_y = current_x, current_y
 
-def update_current_position(event):
-    global current_x, current_y
-    current_x = event.x
-    current_y = event.y
+def stop_drawing(event):
+    global is_drawing
+    is_drawing = False
 
-def draw_line(event):
-    global current_x, current_y
-    canvas.create_line((current_x, current_y, event.x, event.y), width=get_line_width(), fill=drawing_color, capstyle=tk.ROUND, smooth=True)
-    current_x, current_y = event.x, event.y
-
-def select_color(new_color):
+def change_pen_color():
     global drawing_color
-    drawing_color = new_color
+    color = askcolor()[1]
+    if color:
+        drawing_color = color
 
-def clear_canvas():
-    canvas.delete('all')
-    display_color_palette()
+def change_line_width(value):
+    global line_width
+    line_width = int(value)
 
-color_box = tk.PhotoImage(file="./assets/side_bar.png")
-color_box_label = tk.Label(app, image=color_box, bg="#f2f3f5")
-color_box_label.place(x=10, y=20)
+root = tk.Tk()
+root.title("Enhanced Drawing App")
 
-eraser = tk.PhotoImage(file="./assets/eraser.png")
-eraser_button = tk.Button(app, image=eraser, bg="#f2f3f5", command=clear_canvas)
-eraser_button.place(x=30, y=400)
+canvas = tk.Canvas(root, bg="white")
+canvas.pack(fill="both", expand=True)
 
-colors = tk.Canvas(app, bg="#ffffff", width=37, height=300, bd=0)
-colors.place(x=30, y=60)
+is_drawing = False
+drawing_color = "black"
+line_width = 2
 
-def display_color_palette():
-    color_data = {
-        "Black": "black",
-        "Gray": "gray",
-        "Brown": "brown",
-        "Red": "red",
-        "Orange": "orange",
-        "Yellow": "yellow",
-        "Green": "green",
-        "Blue": "blue",
-        "Purple": "purple"
-    }
+root.geometry("800x600")
 
-    for i, (color_name, fill_color) in enumerate(color_data.items()):
-        color_id = colors.create_rectangle(10, 10 + i * 30, 30, 30 + i * 30, fill=fill_color)
-        colors.tag_bind(color_id, "<Button-1>", lambda event, color=fill_color: select_color(color))
+# Create a frame to hold the controls in the same line
+controls_frame = tk.Frame(root)
+controls_frame.pack(side="top", fill="x")
 
-display_color_palette()
+color_button = tk.Button(controls_frame, text="Change Color", command=change_pen_color)
+clear_button = tk.Button(controls_frame, text="Clear Canvas", command=lambda: canvas.delete("all"))
 
-canvas = tk.Canvas(app, width=930, height=500, background="white", cursor="hand2")
-canvas.place(x=100, y=10)
+color_button.pack(side="left", padx=5, pady=5)
+clear_button.pack(side="left", padx=5, pady=5)
 
-canvas.bind("<Button-1>", update_current_position)
-canvas.bind("<B1-Motion>", draw_line)
+line_width_label = tk.Label(controls_frame, text="Line Width:")
+line_width_label.pack(side="left", padx=5, pady=5)
 
-def get_line_width():
-    return '{:.2f}'.format(line_width_value.get())
+line_width_slider = tk.Scale(controls_frame, from_=1, to=10, orient="horizontal", command=lambda val: change_line_width(val))
+line_width_slider.set(line_width)
+line_width_slider.pack(side="left", padx=5, pady=5)
 
-def update_line_width_label(event):
-    line_width_label.configure(text=get_line_width())
+canvas.bind("<Button-1>", start_drawing)
+canvas.bind("<B1-Motion>", draw)
+canvas.bind("<ButtonRelease-1>", stop_drawing)
 
-line_width_value = tk.DoubleVar()
-line_width_slider = ttk.Scale(app, from_=0, to=100, orient='horizontal', command=update_line_width_label, variable=line_width_value)
-line_width_slider.place(x=30, y=530)
-
-line_width_label = ttk.Label(app, text=get_line_width())
-line_width_label.place(x=27, y=550)
-
-app.mainloop()
+root.mainloop()
